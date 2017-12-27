4289d5d588b8f6207b9d966aa00775e6b90018af
baoyujin <bjbaoyujin@corp.netease.com>
2017-12-27 16:16:48 +0800

    创建项目

---
+++ YJCHannelSwitch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YJCHannelSwitch
+//
+//  Created by 包宇津 on 2017/12/27.
+//  Copyright © 2017年 baoyujin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YJCHannelSwitchTests/YJCHannelSwitchTests.m
@@ -0,0 +1,39 @@
+//
+//  YJCHannelSwitchTests.m
+//  YJCHannelSwitchTests
+//
+//  Created by 包宇津 on 2017/12/27.
+//  Copyright © 2017年 baoyujin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface YJCHannelSwitchTests : XCTestCase
+
+@end
+
+@implementation YJCHannelSwitchTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
