41a57ae9776f1e25873d632e630556fc46c7c161
baoyujin <bjbaoyujin@corp.netease.com>
2017-12-28 00:00:34 +0800

    设置CollectionView的header

---
+++ YJChannelControl/YJChannelHeader.h
@@ -0,0 +1,16 @@
+//
+//  YJChannelHeader.h
+//  YJCHannelSwitch
+//
+//  Created by 包宇津 on 2017/12/27.
+//  Copyright © 2017年 baoyujin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YJChannelHeader : UICollectionReusableView
+
+@property (nonatomic, strong) NSString *title;
+@property (nonatomic, strong) NSString *subtitle;
+
+@end
